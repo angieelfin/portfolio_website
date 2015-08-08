236ae84a211fa75699eddaeeb75f273a267a0ef7
Agata Kubiak <aga.kubia@gmail.com>
2015-08-08 13:42:09 +0200

    finished main site

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,14 +2,28 @@
 //= require jquery
 $(document).ready(function(){
 
+// navigation smooth scrolling 
+  $('a[href^="#"]').on('click',function (e) {
+      e.preventDefault();
 
+      var target = this.hash;
+      var $target = $(target);
 
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top
+      }, 200, 'swing', function () {
+          window.location.hash = target;
+      });
+  });
+ 
+
+// menu icon animation
   $('.menu-icon').click(function(){
     $(this).toggleClass('active');
     $('.menu').toggleClass('menu-active-js');
   });
 
-
+// footer icons animation
   $(window).scroll(function(){
     var wScroll = $(this).scrollTop();
 
@@ -21,12 +35,31 @@ $(document).ready(function(){
           }, 150 * (i+1));
         });
       }
+// button appearing when scroll into section
+    var sectionNames = ['section-1-header', 'section-2-about'];
+    var i;
 
+    for (i = 0; i < sectionNames.length; i++) {
 
-    if(wScroll > $('.section-2-about').offset().top - 50) {
-      $('.white-letters').addClass('sea-green-letters');
-      console.log('hi');
+      if(wScroll > $('.'+sectionNames[i]).offset().top) {
+        $('.'+sectionNames[i] + ' .ghost-button').addClass('btn-appear');
+      }
     }
 
+
+
   });
+  
+  $('.view-work').on('click',function (e) {
+      e.preventDefault();
+      $('.work-subsections').slideToggle();
+
+        var target = this.hash;
+        var $target = $('a[href="#subsections"]');
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top + 170
+        }, 200, 'swing');
+    });
+
 });
