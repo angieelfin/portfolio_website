163d219211a066cda129cf1ae450ebb559f4d6d3
Agata Kubiak <aga.kubia@gmail.com>
2015-09-02 18:39:33 +0200

    Add a few changes in code and new scripts

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,44 +4,55 @@ $(document).ready(function(){
 
 // navigation smooth scrolling 
   $('a[href^="#"]').on('click',function (e) {
-      e.preventDefault();
+    e.preventDefault();
 
-      var target = this.hash;
-      var $target = $(target);
+    var target = this.hash;
+    var $target = $(target);
 
-      $('html, body').stop().animate({
-          'scrollTop': $target.offset().top
-      }, 200, 'swing', function () {
-          window.location.hash = target;
-      });
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 350, 'swing', function () {
+        window.location.hash = target;
+    });
   });
  
 
 // menu icon animation
-
-  $('.menu-button').on('click', function(){
+  $('.js-menu-button').on('click', function(){
     $(this).toggleClass('active');
-    $('.menu').toggleClass('js-menu-active');
+    $('.menu').toggleClass('menu-active');
   });
+
+  $(window).scroll(function(){
+    var scrollBottom = $(document).height() - ($(window).height() + $(window).scrollTop());
+    console.log(scrollBottom);
+    if(scrollBottom == 20) {
+      $('.icon-circle').each(function(i){
+        $(this).eq(i).animate({
+        left: "20px",
+        opacity: 1
+      }, "slow", "easein");
+
+    )}
+  )};
 
 // footer icons animation
   $(window).scroll(function(){
     var wScroll = $(this).scrollTop();
 
-    if(wScroll > $('.section-footer').offset().top - ($(window).height()/1.7)) {
+    if(wScroll > $('.footer').offset().top - ($(window).height()/1.7)) {
         $('.icon-circle').each(function(i){
-
           setTimeout(function(){
             $('.icon-circle').eq(i).addClass('is-showing');
           }, 150 * (i+1));
         });
       }
+
 // button appearing when scroll into section
-    var sectionNames = ['section-header', 'section-about'];
+    var sectionNames = ['header', 'section-about'];
     var i;
 
     for (i = 0; i < sectionNames.length; i++) {
-
       if(wScroll > $('.'+sectionNames[i]).offset().top) {
         $('.'+sectionNames[i] + ' .ghost-button').addClass('btn-appear');
       }
@@ -50,19 +61,23 @@ $(document).ready(function(){
   });
 
 // galleries
-  $('.thumb-box').on('click', function(e) {
+  $('.thumb').on('click', function(e) {
     e.preventDefault();
     var id = $(this).attr('data-id');
     var img = $(this).attr('src');
-
-      $('.gallery').append('<div class="gallery-active"><img src="' + img + '" /><span class="close"">&times;</span></div>');
+    $('.gallery').append('<div class="gallery-active"><img src="' + img + '" /><span class="button-close"">&times;</span></div>');
   });
 
-  $('.gallery').delegate('.close', 'click', function() {
-    $('.gallery-active').addClass('inactive');
-    setTimeout(function() {
-      $('.gallery-active').remove();
-    }, 200);
+  $('.gallery').delegate('.button-close', 'click', function() {
+    $('.gallery-active').animate({
+      transform: scale(.05)
+    }, 200, function(){
+       $('.gallery-active').remove();
+    });
+    // .addClass('inactive');
+    // setTimeout(function() {
+    //   $('.gallery-active').remove();
+    // }, 200);
   });
    
 // form validation
@@ -87,7 +102,4 @@ $(document).ready(function(){
             form.submit();
         }
     });
-
 });
-
-
